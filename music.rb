d00bdcc38ade5cac43f87c5db233fab80a86fc4e
jadychan <jadychan123@gmail.com>
2018-06-06 07:20:07 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -3,7 +3,8 @@
 # This method should accept a hash as an argument.
 def list_artists(hash)
   hash.each_key {
-
+    |artist|
+    return artist
   }
 end
 #2. Define a method named `list_songs`. This method should use the `each_value` method to iterate over the music library hash and print out the name of the songs in the library.
